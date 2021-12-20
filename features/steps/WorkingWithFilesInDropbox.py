a183670ef4b451568ef33a2f5b521d60810f94d0
MultiSmith <koval.maksym@gmail.com>
2021-12-20 16:34:17 +0200

    0 added projects

--- features/steps/WorkingWithFilesInDropbox.py
+++ features/steps/WorkingWithFilesInDropbox.py
@@ -1,60 +1,84 @@
 from behave import *
-import requests
-
-#UPLOAD
-
-@given(u'I have some "{file}" to upload to a "{directory}"')
-def step_impl(context, file, directory):
-    f = open(file, "rb")
-    context.png = f.read()
-    context.headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer dSS6on6CQ_AAAAAAAAAAAXDWQTN5tv_RtTqGazhbFS8KSt09Xmh27Y-InA0Q7LlW",
-            "Dropbox-API-Arg": '{"path":"' + directory + '","mode": "add"}'
-    }
-
-@when(u'I send a POST request for upload to Dropbox "{endpoint}"')
-def step_impl(context, endpoint):
-    context.response = requests.post(url = endpoint, headers = context.headers, data = context.png)
-
-@then(u'the file is uploaded and the response is "200 OK"')
-def step_impl(context):
-    assert context.response.status_code == 200, print(context.response)
+import sys
+import os
+from Builder import UploadBuilder, GetFileMetadataBuilder, DeleteBuilder
 
 
-#GET FILE METADATA
+class Upload():
+    def __init__(self):
+        self.Upload = UploadBuilder()
 
-@given(u'I want to see "{file}" metadata')
-def step_impl(context, file):
-    context.headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer dSS6on6CQ_AAAAAAAAAAAXDWQTN5tv_RtTqGazhbFS8KSt09Xmh27Y-InA0Q7LlW"
-    }
-    context.body = '{"file": "' + file + '", "actions": []}'
+    @given(u'I have some "{file}" to upload to a "{directory}"')
+    def set_headers_body(self, file, directory):
+        self.Upload.set_headers({
+                "Content-Type": "application/octet-stream",
+                "Authorization": "Bearer L12J_8DNkQYAAAAAAAAAAdQov2saKF98tz3bY0wje-JCnPKa2tkAGd-v7V0SFv1N",
+                "Dropbox-API-Arg": '{"path":"' + directory + '","mode": "add"}'
+        })
+        f = open(file, "rb")
+        self.Upload.set_body(f.read())
 
-@when(u'I send a POST request for file metadata to Dropbox "{endpoint}"')
-def step_impl(context, endpoint):
-    context.response = requests.post(url = endpoint, headers = context.headers, data = context.body)
+    @when(u'I send a POST request for upload to Dropbox "{endpoint}"')
+    def set_request(self, endpoint):
+        self.response = self.Upload.set_request(endpoint)
+    
+    @then(u'the file is uploaded and the response is "200 OK"')
+    def get_response(self):
+        assert self.Upload.get_response().status_code == 200, print(self.Upload.get_response())
 
-@then(u'the metadata is shown and the response is "200 OK"')
-def step_impl(context):
-    assert context.response.status_code == 200, print(context.response)
+    def testing(self):
+        self.set_headers_body("Museum-art-deco.png", "/WebAPI_HomeTask/Museum-art-deco.png")
+        self.set_request("https://content.dropboxapi.com/2/files/upload")
+        self.get_response()
 
 
-# DELETE
+class GetFileMetadata():
+    def __init__(self):
+        self.GetFileMetadata = GetFileMetadataBuilder()
+    
+    @given(u'I want to see "{file}" metadata')
+    def set_headers_body(self, file):
+        self.GetFileMetadata.set_headers({
+                "Content-Type": "application/json",
+                "Authorization": "Bearer L12J_8DNkQYAAAAAAAAAAdQov2saKF98tz3bY0wje-JCnPKa2tkAGd-v7V0SFv1N"
+        })
+        self.GetFileMetadata.set_body('{"file": "' + file + '", "actions": []}')
 
-@given(u'I want to delete some "{file}"')
-def step_impl(context, file):
-    context.headers = {
-            "Content-Type": "application/octet-stream",
-            "Authorization": "Bearer dSS6on6CQ_AAAAAAAAAAAXDWQTN5tv_RtTqGazhbFS8KSt09Xmh27Y-InA0Q7LlW"
-    }
-    context.body = '{"path": "' + file + '"}'
+    @when(u'I send a POST request for file metadata to Dropbox "{endpoint}"')
+    def set_request(self, endpoint):
+        self.GetFileMetadata.set_request(endpoint)
 
-@when(u'I send a POST request for delete to Dropbox "{endpoint}"')
-def step_impl(context, endpoint):
-    context.response = requests.post(url = endpoint, headers = context.headers, data = context.body)
+    @then(u'the metadata is shown and the response is "200 OK"')
+    def get_response(self):
+        assert self.GetFileMetadata.get_response().status_code == 200, print(self.GetFileMetadata.get_response())
 
-@then(u'the file is deleted and the response is "200 OK"')
-def step_impl(context):
-    assert context.response.status_code == 200, print(context.response)
+    def testing(self):
+        self.set_headers_body("/WebAPI_HomeTask/Museum-art-deco.png")
+        self.set_request("https://api.dropboxapi.com/2/sharing/get_file_metadata")
+        self.get_response()
+
+
+class Delete():
+    def __init__(self):
+        self.Delete = DeleteBuilder()
+    
+    @given(u'I want to delete some "{file}"')
+    def set_headers_body(self, file):
+        self.Delete.set_headers({
+                "Content-Type": "application/json",
+                "Authorization": "Bearer L12J_8DNkQYAAAAAAAAAAdQov2saKF98tz3bY0wje-JCnPKa2tkAGd-v7V0SFv1N"
+        })
+        self.Delete.set_body('{"path": "' + file + '"}')
+
+    @when(u'I send a POST request for delete to Dropbox "{endpoint}"')
+    def set_request(self, endpoint):
+        self.Delete.set_request(endpoint)
+
+    @then(u'the file is deleted and the response is "200 OK"')
+    def get_response(self):
+        assert self.Delete.get_response().status_code == 200, print(self.Delete.get_response())
+
+    def testing(self):
+        self.set_headers_body("/WebAPI_HomeTask/Museum-art-deco.png")
+        self.set_request("https://api.dropboxapi.com/2/files/delete_v2")
+        self.get_response()
